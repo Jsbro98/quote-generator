92e5f77bb7c41ccb7cf5466b45a03d42d3a3fed4
Jacob Harwood <96179296+Jsbro98@users.noreply.github.com>
2026-03-11 18:44:58 -0500

    impl: added first getter method for zenquotes api

--- src/main/java/io/github/jsbro98/quotegenerator/QuoteService.java
+++ src/main/java/io/github/jsbro98/quotegenerator/QuoteService.java
@@ -1,8 +1,19 @@
 package io.github.jsbro98.quotegenerator;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
 
 @Service
 public class QuoteService {
-  
+  private final RestClient restClient = RestClient.create();
+  private final String baseUrl = "https://zenquotes.io/api/random";
+
+  public String getQuote() {
+    return restClient.get()
+            .uri(baseUrl)
+            .accept(MediaType.APPLICATION_JSON)
+            .retrieve()
+            .body(String.class);
+  }
 }
