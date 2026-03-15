0d1e5800ab17c1f28820d3623c6ecc2a766a7ae8
Jacob Harwood <96179296+Jsbro98@users.noreply.github.com>
2026-03-15 16:56:52 -0500

    fix: added @SpringBootTest to fix service test

--- src/test/java/io/github/jsbro98/quotegenerator/QuoteServiceTest.java
+++ src/test/java/io/github/jsbro98/quotegenerator/QuoteServiceTest.java
@@ -2,10 +2,12 @@ package io.github.jsbro98.quotegenerator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@SpringBootTest
 public class QuoteServiceTest {
 
   @Autowired
