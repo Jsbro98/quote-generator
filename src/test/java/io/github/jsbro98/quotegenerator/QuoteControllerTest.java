9db1625ffd6f890d38c7dad1df49cfa208e370a8
Jacob Harwood <96179296+Jsbro98@users.noreply.github.com>
2026-03-15 16:56:33 -0500

    impl: expanded controller test

--- src/test/java/io/github/jsbro98/quotegenerator/QuoteControllerTest.java
+++ src/test/java/io/github/jsbro98/quotegenerator/QuoteControllerTest.java
@@ -3,11 +3,13 @@ package io.github.jsbro98.quotegenerator;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -20,14 +22,16 @@ public class QuoteControllerTest {
   QuoteService quoteService;
 
   @Test
-  public void controllerTest() throws Exception {
+  public void itReturnsEndpointRandom() throws Exception {
     RandomQuote[] testQuoteData = {
-          new RandomQuote("Be brave", "Myself", "html...")
+            new RandomQuote("Be brave", "Myself", "html...")
     };
 
     when(quoteService.getRandomQuote()).thenReturn(testQuoteData);
 
-    mockMvc.perform(get("/quotes/random"))
-            .andExpect(status().isOk());
+    mockMvc.perform(get("/quotes/random")
+                    .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON));
   }
 }
