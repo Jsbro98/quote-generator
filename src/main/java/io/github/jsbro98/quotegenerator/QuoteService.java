6e71546f0dd64abd4d23be6f31f093103ce9fb49
Jacob Harwood <96179296+Jsbro98@users.noreply.github.com>
2026-03-17 18:42:56 -0500

    impl: added QuoteRepository and batch saving method to QuoteService

--- src/main/java/io/github/jsbro98/quotegenerator/QuoteService.java
+++ src/main/java/io/github/jsbro98/quotegenerator/QuoteService.java
@@ -2,16 +2,29 @@ package io.github.jsbro98.quotegenerator;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 @Service
 public class QuoteService {
   private final ZenQuotesClientAPI clientAPI;
+  private final QuoteRepository quoteRepository;
 
-  public QuoteService(ZenQuotesClientAPI clientAPI) {
+  public QuoteService(ZenQuotesClientAPI clientAPI, QuoteRepository quoteRepository) {
     this.clientAPI = clientAPI;
+    this.quoteRepository = quoteRepository;
+    fetchNewQuotes(this.quoteRepository);
   }
 
   public RandomQuote[] getRandomQuote() {
     // TODO: maybe create a DTO for this?
     return clientAPI.randomQuote();
+  }
+
+  private void fetchNewQuotes(QuoteRepository quoteRepository) {
+    ArrayList<QuoteFromListRequest> newData = new ArrayList<>(
+            Arrays.asList(clientAPI.getQuoteBatch()));
+    quoteRepository.resetQuotes();
+    quoteRepository.saveQuotes(newData);
   }
 }
