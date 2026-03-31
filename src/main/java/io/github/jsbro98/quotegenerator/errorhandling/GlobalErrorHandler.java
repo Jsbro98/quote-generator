74f48a63d003309b8198dcc46fffa346be8125ef
Jacob Harwood <96179296+Jsbro98@users.noreply.github.com>
2026-03-31 18:59:32 -0500

    feat: created ControllerAdvice for global error handling

---
+++ src/main/java/io/github/jsbro98/quotegenerator/errorhandling/GlobalErrorHandler.java
@@ -0,0 +1,8 @@
+package io.github.jsbro98.quotegenerator.errorhandling;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalErrorHandler {
+
+}
