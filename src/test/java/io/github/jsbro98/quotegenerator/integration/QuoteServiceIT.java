e0982503c284066e300a56207ac05b9a61529972
Jacob Harwood <96179296+Jsbro98@users.noreply.github.com>
2026-03-30 20:54:27 -0500

    feat: added IT test for QuoteService

---
+++ src/test/java/io/github/jsbro98/quotegenerator/integration/QuoteServiceIT.java
@@ -0,0 +1,49 @@
+package io.github.jsbro98.quotegenerator.integration;
+
+import io.github.jsbro98.quotegenerator.ZenQuoteDTO;
+import io.github.jsbro98.quotegenerator.ZenQuoteRandomDTO;
+import io.github.jsbro98.quotegenerator.service.QuoteService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class QuoteServiceIT {
+
+  @Autowired
+  private QuoteService quoteService;
+
+  @Test
+  public void shouldGetRandomQuote() {
+    ZenQuoteRandomDTO quote = quoteService.getRandomQuote();
+    assertNotNull(quote, "quote must be present");
+  }
+
+  // TODO: refactor this into nested or multiple tests
+  @Test
+  public void shouldGet_QuotesFromBatch() {
+    ZenQuoteDTO quoteOne = quoteService.getRandomSavedQuote();
+    ZenQuoteDTO quoteTwo = quoteService.getRandomSavedQuote();
+    ZenQuoteDTO quoteThree = quoteService.getRandomSavedQuote();
+
+    assertAll("All quotes are not null",
+            () -> assertNotNull(quoteOne),
+            () -> assertNotNull(quoteTwo),
+            () -> assertNotNull(quoteThree)
+    );
+
+    assertAll("All quotes are not the same object",
+            () -> assertNotSame(quoteOne, quoteTwo),
+            () -> assertNotSame(quoteOne, quoteThree),
+            () -> assertNotSame(quoteTwo, quoteThree)
+    );
+
+    assertAll("All quotes must be different",
+            () -> assertNotEquals(quoteOne, quoteTwo),
+            () -> assertNotEquals(quoteOne, quoteThree),
+            () -> assertNotEquals(quoteTwo, quoteThree)
+    );
+  }
+}
