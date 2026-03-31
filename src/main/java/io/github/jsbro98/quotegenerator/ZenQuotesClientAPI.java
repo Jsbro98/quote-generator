01107bebc8c6710d8cc1d429dba2b28a0ba6ec9b
Jacob Harwood <96179296+Jsbro98@users.noreply.github.com>
2026-03-30 20:56:26 -0500

    impl: added null checking to array indexing

--- src/main/java/io/github/jsbro98/quotegenerator/ZenQuotesClientAPI.java
+++ src/main/java/io/github/jsbro98/quotegenerator/ZenQuotesClientAPI.java
@@ -4,6 +4,8 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
+import java.util.Objects;
+
 @Component
 public class ZenQuotesClientAPI {
   private final String baseUrl = "https://zenquotes.io/api";
@@ -16,6 +18,11 @@ public class ZenQuotesClientAPI {
             .accept(MediaType.APPLICATION_JSON)
             .retrieve()
             .body(ZenQuoteRandomDTO[].class);
+
+    Objects.requireNonNull(response, "response must not be null");
+    // ZenQuotes /random API returns an array of JSON with 1 item
+    // so lets return the 1 element in the array
+    return response[0];
   }
 
   public ZenQuoteDTO[] getQuoteBatch() {
