b8f9487c31b123d3d3a6602473f617f642fee5cf
Jacob Harwood <96179296+Jsbro98@users.noreply.github.com>
2026-04-07 00:52:36 -0500

    change: formatting

--- src/main/java/io/github/jsbro98/quotegenerator/errorhandling/GlobalErrorHandler.java
+++ src/main/java/io/github/jsbro98/quotegenerator/errorhandling/GlobalErrorHandler.java
@@ -9,16 +9,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalErrorHandler {
+
   @ExceptionHandler(ZenQuoteBatchFailure.class)
   public ResponseEntity<ApiErrorResponse> handleBatchFailure(ZenQuoteBatchFailure ex) {
-   ApiErrorResponse error = new ApiErrorResponse(400, ex.getMessage(), System.currentTimeMillis());
+    ApiErrorResponse error = new ApiErrorResponse(400, ex.getMessage(), System.currentTimeMillis());
     return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
   }
 
   // A fallback for any unexpected errors that don't get specifically caught
   @ExceptionHandler(Exception.class)
   public ResponseEntity<ApiErrorResponse> handleGenericException(Exception ex) {
-    ApiErrorResponse error = new ApiErrorResponse(500, ex.getMessage(),  System.currentTimeMillis());
+    ApiErrorResponse error = new ApiErrorResponse(500, ex.getMessage(), System.currentTimeMillis());
     return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
   }
 }
