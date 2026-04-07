35c1d5ad7734ea047d9a07b210e416f1d8bb91e9
Jacob Harwood <96179296+Jsbro98@users.noreply.github.com>
2026-04-07 18:24:44 -0500

    feat: added unit testing for GlobalErrorHandler

---
+++ src/test/java/io/github/jsbro98/quotegenerator/unit/GlobalErrorHandlerTest.java
@@ -0,0 +1,35 @@
+package io.github.jsbro98.quotegenerator.unit;
+
+import io.github.jsbro98.quotegenerator.errorhandling.GlobalErrorHandler;
+import io.github.jsbro98.quotegenerator.errorhandling.customerrors.ApiErrorResponse;
+import io.github.jsbro98.quotegenerator.errorhandling.customerrors.ZenQuoteBatchFailure;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class GlobalErrorHandlerTest {
+  private final GlobalErrorHandler globalErrorHandler = new GlobalErrorHandler();
+
+  @Test
+  void shouldHandleBatchException() throws Exception {
+    ZenQuoteBatchFailure exception = new ZenQuoteBatchFailure("Bad Batch!");
+    ResponseEntity<ApiErrorResponse> response = globalErrorHandler.handleBatchFailure(exception);
+
+    assertNotNull(response.getBody());
+    assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    assertEquals("Bad Batch!", response.getBody().message());
+  }
+
+  @Test
+  void shouldHandleGeneralException() throws Exception {
+    Exception exception = new Exception("Bad Exception!");
+    ResponseEntity<ApiErrorResponse> response = globalErrorHandler.handleGenericException(exception);
+
+    assertNotNull(response.getBody());
+    assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+    assertEquals("Bad Exception!", response.getBody().message());
+  }
+}
