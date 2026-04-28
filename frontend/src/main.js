c479f780291f6e50956e98672c232f47c7ec4030
Jacob Harwood <96179296+Jsbro98@users.noreply.github.com>
2026-04-27 23:36:31 -0500

    impl: added basic fetching from java src

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,1 +1,9 @@
-console.log('vite is working!');
+const quoteHolder = document.querySelector('.main-quote');
+const requestButton = document.querySelector('.request-quote-button');
+
+requestButton.addEventListener('click', (e) => {
+    fetch('/quotes/random')
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => console.error(err));
+})
