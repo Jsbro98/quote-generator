d30e4280baacdeccd62763dede439364cff4ce20
Jacob Harwood <96179296+Jsbro98@users.noreply.github.com>
2026-05-08 20:26:43 -0500

    impl: added terser and config options

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,6 +8,23 @@ export default defineConfig({
     },
     build: {
         outDir: '../src/main/resources/static',
-        emptyOutDir: true
-    }
+        emptyOutDir: true,
+        sourcemap: false,
+        minify: 'terser',
+        terserOptions: {
+            compress: {
+                drop_console: true,
+                drop_debugger: true,
+                passes: 2,
+            },
+            mangle: {
+                toplevel: true,
+                properties: true,
+            },
+            format: {
+                comments: false,
+                beautify: false,
+            },
+        },
+    },
 })
